a163737baf6afeeb81c843f52c965c0bc3c8f96e
Tim <50201557+Icola1@users.noreply.github.com>
2022-04-11 00:24:28 +0200

    added kursansicht and Klausurenübersicht

--- frontend/src/components/Klausurenuebersicht.js
+++ frontend/src/components/Klausurenuebersicht.js
@@ -14,13 +14,39 @@ export class Klausurenuebersicht extends Component {
         <Container className="Container" fluid>
           <Row className="Row" fluid>
             <Col xs={2} className="ColNav" fluid><ShowNavbar /></Col>
-            <Col xs={10} className="ColContent" fluid><h1>Klausurenübersicht</h1></Col>
+            <Col xs={10} className="ColContent" fluid>
+              <Row className="Section">
+                <h1>Angemeldete Klausuren</h1>
+                <Col xs={4} fluid> <ShowExam /></Col>
+                <Col xs={4} fluid> <ShowExam /></Col>
+                <Col xs={4} fluid> <ShowExam /></Col>
+                <Col xs={4} fluid> <ShowExam /></Col>
+              </Row>
+              <Row className="Section">
+                <h1>Klausuren aus ihren Kursen</h1>
+                <Col xs={4} fluid> <ShowExam /></Col>
+                <Col xs={4} fluid> <ShowExam /></Col>
+                <Col xs={4} fluid> <ShowExam /></Col>
+                <Col xs={4} fluid> <ShowExam /></Col>
+              </Row>
+            </Col>
           </Row>
         </Container>
       </div>
     </div>
     )
   }
+}
+
+function ShowExam() {
+  return (
+    <div className="Exam">
+      <h4 className='ExamName'>Klausurname</h4>
+      <p className='ExamCourse'>Kurs</p>
+      <p className='ExamOwner'>Klausurersteller</p>
+      <p className='ExamDegree'>Studiengang</p>
+    </div>
+  )
 }
 
 export default Klausurenuebersicht
--- frontend/src/components/Kursansicht.js
+++ frontend/src/components/Kursansicht.js
@@ -14,7 +14,74 @@ export class Kursansicht extends Component {
         <Container className="Container" fluid>
           <Row className="Row" fluid>
             <Col xs={2} className="ColNav" fluid><ShowNavbar /></Col>
-            <Col xs={10} className="ColContent" fluid><h1>Kursansicht</h1></Col>
+            <Col xs={10} className="ColContent" fluid>
+              <h1>Kursname</h1>
+              <p>Termine</p>
+              <p>Kursbeschreibung</p>
+              <a><p>Forum</p></a>
+              <div className="MaterialSection Section">
+                <h2>Material</h2>
+                <div className='MaterialContainer'>
+                <h6>Materialname</h6>
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>https://www.youtube.com/watch?v=dQw4w9WgXcQ</a>
+                </div>
+                
+                <div className='MaterialContainer'>
+                <h6>Materialname</h6>
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>https://www.youtube.com/watch?v=dQw4w9WgXcQ</a>
+                </div>
+
+                <div className='MaterialContainer'>
+                <h6>Materialname</h6>
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>https://www.youtube.com/watch?v=dQw4w9WgXcQ</a>
+                </div>
+              </div>
+
+              <div className="ExerciseSection Section">
+                <h2>Abgaben</h2>
+                <div className='ExerciseContainer'>
+                <h6>Abgabe</h6>
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>https://www.youtube.com/watch?v=dQw4w9WgXcQ</a>
+                </div>
+                
+                <div className='ExerciseContainer'>
+                <h6>Abgabe</h6>
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>https://www.youtube.com/watch?v=dQw4w9WgXcQ</a>
+                </div>
+
+                <div className='ExerciseContainer'>
+                <h6>Abgabe</h6>
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>https://www.youtube.com/watch?v=dQw4w9WgXcQ</a>
+                </div>
+              </div>
+
+              <div className="SurveySection Section">
+                <h2>Umfragen</h2>
+                <div className='ExerciseContainer'>
+                <h6>Umfrage</h6>
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>https://www.youtube.com/watch?v=dQw4w9WgXcQ</a>
+                </div>
+                
+                <div className='ExerciseContainer'>
+                <h6>Umfrage</h6>
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>https://www.youtube.com/watch?v=dQw4w9WgXcQ</a>
+                </div>
+
+                <div className='ExerciseContainer'>
+                <h6>Umfrage</h6>
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>https://www.youtube.com/watch?v=dQw4w9WgXcQ</a>
+                </div>
+              </div>
+
+              <div className="ExamSection Section">
+                <h2>Klausuren</h2>
+                <div className='ExerciseContainer'>
+                <h6>Klausur</h6>
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>https://www.youtube.com/watch?v=dQw4w9WgXcQ</a>
+                </div>
+              </div>
+              
+            </Col>
           </Row>
         </Container>
       </div>
