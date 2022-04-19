fcaaf04df338b7b2d12fd24f8cd29c390bca0807
Tim <50201557+Icola1@users.noreply.github.com>
2022-04-19 13:44:28 +0200

    made kursuebersicht dynamic

--- frontend/src/components/Kursuebersicht.js
+++ frontend/src/components/Kursuebersicht.js
@@ -6,9 +6,49 @@ import { ShowHeader, ShowNavbar } from './App'
 import '../css/App.css';
 import '../css/Kursübersicht.css'
 export class Kursuebersicht extends Component {
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+      UserRights: true, // true if active user can create courses
+
+      MyCourses: [{ CourseName: "test", CourseOwner: "abcs", CourseBio: "asdf", CourseDegree: "AI", CourseCreatedAt: "hre" }, { CourseName: "dhj", CourseOwner: "fgj", CourseBio: "dghjm", CourseCreatedAt: "hg" }],
+
+      CoursesTaken: [{ CourseName: "dhj", CourseOwner: "fgj", CourseBio: "dghjm", CourseCreatedAt: "hg" }],
+
+      CoursesSuggested: [{ CourseName: "sgfh", CourseOwner: "sgjf", CourseBio: "fgj", CourseCreatedAt: "dj" }],
+
+
+
+    }
+  }
+
 
   render() {
+    var MyCourseslist = []
+    for (const Course of this.state.MyCourses) {
+      MyCourseslist.push(<Col xs={4} fluid><ShowCourse CourseName={Course.CourseName} 
+        CourseOwner={Course.CourseOwner} CourseBio={Course.CourseBio} 
+        CourseDegree={Course.CourseDegree} CourseCreatedAt={Course.CourseCreatedAt} /></Col>)
+    }
+
+    var CoursesTakenlist = []
+    for (const Course of this.state.CoursesTaken) {
+      CoursesTakenlist.push(<Col xs={4} fluid><ShowCourse CourseName={Course.CourseName}
+        CourseOwner={Course.CourseOwner} CourseBio={Course.CourseBio}
+        CourseDegree={Course.CourseDegree} CourseCreatedAt={Course.CourseCreatedAt} /></Col>)
+    }
+
+    var CoursesSuggestedlist = []
+    for (const Course of this.state.CoursesSuggested) {
+      CoursesSuggestedlist.push(<Col xs={4} fluid><ShowCourse CourseName={Course.CourseName}
+        CourseOwner={Course.CourseOwner} CourseBio={Course.CourseBio}
+        CourseDegree={Course.CourseDegree} CourseCreatedAt={Course.CourseCreatedAt} /></Col>)
+    }
+
+
+
     return (
       <div className="Kursuebersicht">
         <ShowHeader />
@@ -17,27 +57,22 @@ export class Kursuebersicht extends Component {
             <Row className="Row" fluid>
               <Col xs={2} className="ColNav" fluid><ShowNavbar /></Col>
               <Col xs={10} className="ColContent" fluid>
+                <Row className="Section" hidden={!this.state.UserRights}>
+                  <h1>Meine Kurse</h1>
+                  {MyCourseslist}
+
+                  <input type="submit" value="Kurs erstellen" />
+                </Row>
                 <Row className="Section">
-                  <h1>Kursübersicht</h1>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
+                  <h1>Belegte Kurse</h1>
+                  {CoursesTakenlist}
                 </Row>
                 <Row className="Section">
                   <h1>Vorgeschlagene Kurse</h1>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
+                  {CoursesSuggestedlist}
+
                 </Row>
-                <Row className="Section">
-                  <h1>Meine Kurse</h1>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
-                  <Col xs={4} fluid> <ShowCourse /></Col>
-                </Row>
+
               </Col>
             </Row>
           </Container>
@@ -47,13 +82,15 @@ export class Kursuebersicht extends Component {
   }
 }
 
-function ShowCourse() {
+function ShowCourse(props) {
+
   return (
     <div className="Course">
-      <h4 className='CourseName'>Kursname</h4>
-      <p className='CourseOwner'>Kursersteller</p>
-      <p className='CourseDeskription'>Kursbeschreibung</p>
-      <p className='CourseDegree'>Studiengang</p>
+      <h4 className='CourseName'>{props.CourseName}</h4>
+      <p className='CourseOwner'>Kursbesitzer: {props.CourseOwner}</p>
+      <p className='CourseDeskription'>{props.CourseBio}</p>
+      <p className='CourseDegree'>Studiengang: {props.CourseDegree}</p>
+      <p className='CourseCreatedAt'>erstellt am:{props.CourseCreatedAt}</p>
     </div>
   )
 }
