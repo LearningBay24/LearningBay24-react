546ce3b928cda4ec4e3b6db1c4e421b9b26d2797
Tim <50201557+Icola1@users.noreply.github.com>
2022-06-12 12:31:44 +0200

    added "Klausurenübersicht" functionality

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -360,3 +360,51 @@ export function getAppointments(caller, callback) {
       })
       .catch((error) => console.error(error));
 }
+
+export function getAttendedExams(caller) {
+  console.log("(getExams): " + Actualadress + "exams/attended");
+
+  fetch(Actualadress + "exams/attended", {method: "GET",
+    credentials: "include"})
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        caller.setState({AttendedExams: data});
+      });
+}
+
+export function getPassedExams(caller) {
+  console.log("(getExams): " + Actualadress + "exams/passed");
+
+  fetch(Actualadress + "exams/passed", {method: "GET",
+    credentials: "include"})
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        caller.setState({PassedExams: data});
+      });
+}
+
+export function getCreatedExams(caller) {
+  console.log("(getExams): " + Actualadress + "exams/created");
+
+  fetch(Actualadress + "exams/created", {method: "GET",
+    credentials: "include"})
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        caller.setState({CreatedExams: data});
+      });
+}
+
+export function getExams(caller) {
+  console.log("(getExams): " + Actualadress + "exams");
+
+  fetch(Actualadress + "users/exams", {method: "GET",
+    credentials: "include"})
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        caller.setState({RegisteredExams: data});
+      });
+}
--- frontend/src/components/Klausurenuebersicht.js
+++ frontend/src/components/Klausurenuebersicht.js
@@ -1,13 +1,172 @@
+/* eslint-disable react/prop-types */
 import React, {Component} from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import {ShowHeader, ShowNavbar} from "./App";
 import {ShowFooter} from "./Footer";
+import {
+  getAttendedExams, getCreatedExams,
+  getPassedExams, getExams,
+} from "../api";
+
+import {Link} from "react-router-dom";
 
 import "../css/Overlay.css";
 import "../css/Klausurenuebersicht.css";
 
 export class Klausurenuebersicht extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      UnregisteredExams: [{
+        id: 1,
+        name: "",
+        description: "",
+        date: "",
+        duration: "",
+        online: "",
+        location: "",
+        course_id: "",
+        creator_id: "",
+        graded: "",
+        register_deadline: "",
+        deregister_deadline: "",
+        created_at: "",
+        updated_at: "",
+        deleted_at: "",
+      }],
+
+      RegisteredExams: [{
+        id: 1,
+        name: "",
+        description: "",
+        date: "",
+        duration: "",
+        online: "",
+        location: "",
+        course_id: "",
+        creator_id: "",
+        graded: "",
+        register_deadline: "",
+        deregister_deadline: "",
+        created_at: "",
+        updated_at: "",
+        deleted_at: "",
+      }],
+
+      AttendedExams: [{
+        id: 1,
+        name: "",
+        description: "",
+        date: "",
+        duration: "",
+        online: "",
+        location: "",
+        course_id: "",
+        creator_id: "",
+        graded: "",
+        register_deadline: "",
+        deregister_deadline: "",
+        created_at: "",
+        updated_at: "",
+        deleted_at: "",
+      }],
+
+      CreatedExams: [{
+        id: 1,
+        name: "",
+        description: "",
+        date: "",
+        duration: "",
+        online: "",
+        location: "",
+        course_id: "",
+        creator_id: "",
+        graded: "",
+        register_deadline: "",
+        deregister_deadline: "",
+        created_at: "",
+        updated_at: "",
+        deleted_at: "",
+      }],
+
+      PassedExams: [{
+        id: 1,
+        name: "",
+        description: "",
+        date: "",
+        duration: "",
+        online: "",
+        location: "",
+        course_id: "",
+        creator_id: "",
+        graded: "",
+        register_deadline: "",
+        deregister_deadline: "",
+        created_at: "",
+        updated_at: "",
+        deleted_at: "",
+      }],
+    };
+  }
+
+  componentDidMount() {
+    getAttendedExams(this);
+    getCreatedExams(this);
+    getPassedExams(this);
+    getExams(this);
+  }
   render() {
+    const unregisteredList = [];
+    if (this.state.UnregisteredExams != null) {
+      for (const Exam of this.state.UnregisteredExams) {
+        unregisteredList.push(<Col xs={4} fluid><ShowExam name={Exam.name}
+          creator_id={Exam.creator_id}
+          description={Exam.description}
+          register_deadline={Exam.register_deadline}
+          deregister_deadline={Exam.deregister_deadline}
+          date={Exam.date} /></Col>);
+      }
+    }
+
+    const registeredList = [];
+    if (this.state.RegisteredExams != null) {
+      for (const Exam of this.state.RegisteredExams) {
+        registeredList.push(<Col xs={4} fluid><ShowExam name={Exam.name}
+          creator_id={Exam.creator_id}
+          description={Exam.description}
+          register_deadline={Exam.register_deadline}
+          deregister_deadline={Exam.deregister_deadline}
+          date={Exam.date} /></Col>);
+      }
+    }
+
+
+    const attendedList = [];
+    if (this.state.AttendedExams != null) {
+      for (const Exam of this.state.AttendedExams) {
+        let grade = Exam.graded;
+        if (grade === 0 || grade === null) {
+          grade = "Noch nicht bewertet";
+        }
+        attendedList.push(<Col xs={4} fluid><ShowAttendedExam name={Exam.name}
+          creator_id={Exam.creator_id}
+          description={Exam.description}
+          graded={grade}
+          date={Exam.date} /></Col>);
+      }
+    }
+
+    const passedList = [];
+    if (this.state.PassedExams != null) {
+      for (const Exam of this.state.PassedExams) {
+        passedList.push(<Col xs={4} fluid><ShowAttendedExam name={Exam.name}
+          creator_id={Exam.creator_id}
+          description={Exam.description}
+          graded={Exam.graded}
+          date={Exam.date} /></Col>);
+      }
+    }
     return (
       <div className="Klausurenuebersicht">
         <ShowHeader />
@@ -28,56 +187,59 @@ export class Klausurenuebersicht extends Component {
                   </button>
                 </div>
                 <Row className="Section">
-                  <h2>Angemeldete Klausuren</h2>
-                  <div className="ExamWrapper"> <ShowExam /></div>
-                  <div className="ExamWrapper"> <ShowExam /></div>
-                  <div className="ExamWrapper"> <ShowExam /></div>
-                  <div className="ExamWrapper"> <ShowExam /></div>
+                  <h1>Angemeldete Klausuren</h1>
+                  {registeredList}
                 </Row>
                 <Row className="Section">
-                  <h2>Anmelden</h2>
-                  <div className="ExamWrapper"> <ShowExam /></div>
-                  <div className="ExamWrapper"> <ShowExam /></div>
-                  <div className="ExamWrapper"> <ShowExam /></div>
-                  <div className="ExamWrapper"> <ShowExam /></div>
+                  <h1>Anmelden</h1>
+                  {unregisteredList}
                 </Row>
                 <Row className="Section">
-                  <h2>Vergangene Klausuren</h2>
-                  <div className="ExamWrapper"> <ShowExam /></div>
-                  <div className="ExamWrapper"> <ShowExam /></div>
-                  <div className="ExamWrapper"> <ShowExam /></div>
-                  <div className="ExamWrapper"> <ShowExam /></div>
+                  <h1>Vergangene Klausuren</h1>
+                  <Row className="Section">
+                    <h1>Teilgenommene Klausuren</h1>
+                    {attendedList}
+                  </Row>
+                  <Row className="Section">
+                    <h1>Bestandene Klausuren</h1>
+                    {passedList}
+                  </Row>
                 </Row>
               </Col>
             </Row>
           </Container>
         </div>
-        <ShowFooter/>
+        <ShowFooter />
       </div>
     );
   }
 }
 
-function ShowExam() {
+function ShowAttendedExam(props) {
   return (
-    <div className="Exam">
-      <div className="ExamHeader">
-        <p className="ExamName">Klausurname</p>
+    <Link to={"/Klausuransicht/" + props.exam_id}>
+      <div className="Exam">
+        <h4 className="ExamName">{props.name}</h4>
+        <p className="ExamOwner">Prüfer:{props.creator_id}</p>
+        <p className="ExamDescription">{props.description}</p>
+        <p className="ExamDate">{props.date}</p>
+        <p className="ExamGraded">Note: {props.graded}</p>
       </div>
-      <div className="ExamBody">
-        <div className="ExamCourseInfo">
-          <p className="ExamCourse">Kursname</p>
-          <p className="ExamDegree">Studiengang</p>
-          <p className="ExamOwner">Klausurersteller</p>
-        </div>
-        <div className="ExamInfo">
-          <p className="ExamDate">Datum</p>
-          <p className="ExamTime">Uhrzeit</p>
-          <p className="ExamDuration">Dauer</p>
-          <p className="ExamRoom">Raum</p>
-        </div>
+    </Link>
+  );
+}
+
+function ShowExam(props) {
+  return (
+    <Link to={"/Klausuransicht/" + props.exam_id}>
+      <div className="Exam">
+        <h4 className="ExamName">{props.name}</h4>
+        <p className="ExamDescription">{props.description}</p>
+        <p className="Examduration">Dauer :{props.duration}min</p>
+        <p className="ExamDate">{props.date}</p>
+        <p className="ExamRoom">{props.location}</p>
       </div>
-    </div>
+    </Link>
   );
 }
 
